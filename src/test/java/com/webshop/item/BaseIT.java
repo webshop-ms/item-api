c49ffc89d05e07dab0943420a0aebf496b57caab
Szabolcs Torma <szabolcs.torma@accretivetg.com>
2022-08-02 07:17:45 +0200

    undo properties based test container

--- src/test/java/com/webshop/item/BaseIT.java
+++ src/test/java/com/webshop/item/BaseIT.java
@@ -1,16 +1,24 @@
 package com.webshop.item;
 
-import org.junit.ClassRule;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @ActiveProfiles("test")
 @Testcontainers
 public class BaseIT {
 
-    @ClassRule
-    static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:latest")
-        .withDatabaseName("testitemdb");
+    @Container
+    static MySQLContainer<?> mysql = new MySQLContainer<>("mysql:latest");
+
+    @DynamicPropertySource
+    static void mysqlProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", mysql::getJdbcUrl);
+        registry.add("spring.datasource.username", mysql::getUsername);
+        registry.add("spring.datasource.password", mysql::getPassword);
+    }
 
 }
